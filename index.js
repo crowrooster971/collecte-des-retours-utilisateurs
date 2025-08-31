489b4cad15b3f678cc5deb561d5e5860594ce61d
crowrooster971 <wanlishajna@hotmail.com>
2025-08-31 18:02:32 +0300

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l\'application Collecte des Retours Utilisateurs');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
